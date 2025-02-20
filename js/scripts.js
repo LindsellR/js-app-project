4b410cc13d83211dd5c088aecd191e5bd8891590
LindsellR <lindsellrichard@gmail.com>
2025-02-20 15:07:16 +1100

    scripts.js changed pokemonList array
    scripts.js
    As pokemonList is an array of pokemon characters needing a nested array with at least 2 types, I changed squirtle and charmander to 'butterfree' and 'charizard' as they each have two type descriptors creating a nested array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
 let pokemonList = [{ name: 'bulbasaur', height: 7, type: ['grass, poison'] },
-{ name: 'squirtle', height: 2, type: 'water' },
-{ name: 'charmander', height: 7, type: 'fire' }
+    { name: 'butterfree', height: 3, type: ['bug', 'flying'] },
+    { name: 'charizard', height: 5, type: ['fire', 'flying'] }
 ];
