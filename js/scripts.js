b0bfe5e80f0a23fc96d266299ac6bdfaf1edb3f2
LindsellR <lindsellrichard@gmail.com>
2025-02-21 18:08:46 +1100

    scripts.js - added loop, css-new styles, html tags
    script.js
    added a loop to iterate over pokemonList to show pokemon data on DOM. Added html tags and classes to style with css.
    
    css
    added initial styling to pokedex with postprocessing and through auto prefixer.
    Added new css file for postprocessed css
    HTML
    added h1, header and classes to style css

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,13 @@
-let pokemonList = [{ name: 'bulbasaur', height: 7, type: ['grass, poison'] },
-    { name: 'butterfree', height: 3, type: ['bug', 'flying'] },
-    { name: 'charizard', height: 5, type: ['fire', 'flying'] }
+let pokemonList = [
+  { name: 'Bulbasaur', height: 7, type: ['grass, poison'] },
+  { name: 'Butterfree', height: 3, type: ['bug', 'flying'] },
+  { name: 'Charizard', height: 5, type: ['fire', 'flying'] }
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height > 5) {
+    document.write('<div class="card"><span class="list-item">' + pokemonList[i].name + '</span> Height: ' + pokemonList[i].height + '. <br> That\'s a big Pokemon!</div>');
+  } else {
+    document.write('<div class="card"><span class="list-item">' + pokemonList[i].name + '</span> Height: ' + pokemonList[i].height + '</div>');
+  }
+}
