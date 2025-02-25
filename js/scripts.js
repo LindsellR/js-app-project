d1cc13f2883f7a942078f7eb8456e12c94ecace3
LindsellR <lindsellrichard@gmail.com>
2025-02-25 13:10:18 +1100

    Updated .js functions to forEach and IIFE
    scripts.js
    updated the javascript functions to for.Each loop and wrapped the pokemonList in IIFE to remove from global accessibility. Also used typeOf as a conditional to the add(pokemon) function to make sure only objects are added.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,46 @@
-let pokemonList = [
-  { name: 'Bulbasaur', height: 7, type: ['grass, poison'] },
-  { name: 'Butterfree', height: 3, type: ['bug', 'flying'] },
-  { name: 'Charizard', height: 5, type: ['fire', 'flying'] }
-];
+let pokemonRepository = (function () {
+  let pokemonList = [
+    { name: 'Bulbasaur', height: 7, type: ['grass, poison'] },
+    { name: 'Butterfree', height: 3, type: ['bug', 'flying'] },
+    { name: 'Charizard', height: 5, type: ['fire', 'flying'] }
+  ];
 
-//loops through pokemonList variable
-for (let i = 0; i < pokemonList.length; i++) {
-  //extracts certain data and prints to DOM. html tags added for styling
-  if (pokemonList[i].height > 5) {
-    document.write('<div class="card"><span class="list-item">' + pokemonList[i].name + '</span> Height: ' + pokemonList[i].height + '. <br> That\'s a big Pokemon!</div>');
-  } else {
-    document.write('<div class="card"><span class="list-item">' + pokemonList[i].name + '</span> Height: ' + pokemonList[i].height + '</div>');
+  function add(pokemon) {
+    if (typeof pokemon === 'object' && pokemon !== null
+    ) {
+      pokemonList.push(pokemon);
+    } else {
+      alert('Invalid input');
+    }
   }
-}
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+
+
+// //loops through pokemonList variable
+// for (let i = 0; i < pokemonList.length; i++) {
+//   //extracts certain data and prints to DOM. html tags added for styling
+//   if (pokemonList[i].height > 5) {
+//     document.write('<div class="card"><span class="list-item">' + pokemonList[i].name + '</span> Height: ' + pokemonList[i].height + '. <br> That\'s a big Pokemon!</div>');
+//   } else {
+//     document.write('<div class="card"><span class="list-item">' + pokemonList[i].name + '</span> Height: ' + pokemonList[i].height + '</div>');
+//   }
+// }
+
+pokemonRepository.getAll().forEach(function (pokemon) {
+  //extracts certain data and prints to DOM. html tags added for styling
+  if (pokemon.height > 5) {
+    document.write('<div class="card"><span class="list-item">' + pokemon.name + '</span> Height: ' + pokemon.height + '. <br> That\'s a big Pokemon!</div>');
+  } else {
+    document.write('<div class="card"><span class="list-item">' + pokemon.name + '</span> Height: ' + pokemon.height + '</div>');
+  }
+});
+
