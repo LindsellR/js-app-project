97f228318a616f9a39490084ee5015a6297c2448
LindsellR <lindsellrichard@gmail.com>
2025-02-26 18:59:32 +1100

    styles.css, html, scripts.js
    styles.css
    Added button styling and hover state for buttons. I widened the buttons and added 10px to the margin. Adjusted the header items for responsiveness
    
    index.html
    removed superceded css classes from main. Added ul and class for js.
    
    scripts.js
    added function to create li element buttons for pokemon with event listener and function to show details,
    used add(pokemon function to add new pokemon and pushed to list. Commented out Superceded code.

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,64 @@
+//Pokemon Array wrapped in IIFE
+
 let pokemonRepository = (function () {
   let pokemonList = [
     { name: 'Bulbasaur', height: 7, type: ['grass, poison'] },
     { name: 'Butterfree', height: 3, type: ['bug', 'flying'] },
-    { name: 'Charizard', height: 5, type: ['fire', 'flying'] }
+    { name: 'Charizard', height: 5, type: ['fire', 'flying'] },
   ];
 
-  function add(pokemon) {
-    if (typeof pokemon === 'object' && pokemon !== null
-    ) {
-      pokemonList.push(pokemon);
-    } else {
-      alert('Invalid input');
-    }
-  }
+  //Function to add list item button
+  function addListItem(pokemon){
+    let listContainer = document.querySelector('.pokemon-list');
+    let listItem = document.createElement('li');
+    let button = document.createElement('button');
+    button.innerText = pokemon.name;
+    button.classList.add('pokeButton');
+    listItem.appendChild(button);
+    listContainer.appendChild(listItem)
 
+    button.addEventListener('click', function(){
+      showDetails(pokemon);
+  });
+
+  //To remove bullet points from list
+  listContainer.style.listStyleType = 'none';
+
+    //function to show pokemon details
+   function showDetails(pokemon){
+     console.log(pokemon);
+  }
+  }
+  // Function to add pokemon items if consitions are met.
+    function add(pokemon) {
+     if (pokemon && typeof pokemon === 'object' && 'name' in pokemon && 'height' in pokemon && 'type' in pokemon)
+    {
+      pokemonList.push(pokemon);
+     } else {
+       console.log('Invalid input: $Json.stringify(pokemon) is not a valid pokemon');
+     }
+   }
+  
   function getAll() {
     return pokemonList;
   }
 
+  // to make pokemon list accessible outside of function
   return {
     add: add,
-    getAll: getAll
+    getAll: getAll,
+    addListItem: addListItem,
   };
 })();
+//adding pokemon to list if conditions are met
+pokemonRepository.add({ name: "Pikachu", height: 0.3, type: ["electric"] });
 
+//forEach loop over pokemon array
+pokemonRepository.getAll().forEach(function (pokemon) {
+  pokemonRepository.addListItem(pokemon)
+});
 
+// *SUPRECEDED CODE!
 // //loops through pokemonList variable
 // for (let i = 0; i < pokemonList.length; i++) {
 //   //extracts certain data and prints to DOM. html tags added for styling
@@ -35,18 +69,10 @@ let pokemonRepository = (function () {
 //   }
 // }
 
-pokemonRepository.getAll().forEach(function (pokemon) {
-  let listContainer = document.querySelector('.pokemon-list');
-  let listItem = document.createElement('li');
-  let button = document.createElement('button');
-  button.innerText = pokemon.name;
-  button.classList.add('pokeButton');
-  listContainer.appendChild(button);
-
   //extracts certain data and prints to DOM. html tags added for styling
 //  if (pokemon.height > 5) {
 //  document.write('<div class="card"><span class="list-item">' + pokemon.name + '</span> Height: ' + pokemon.height + '. <br> That\'s a big Pokemon!</div>');
 // } else {
 //  document.write('<div class="card"><span class="list-item">' + pokemon.name + '</span> Height: ' + pokemon.height + '</div>');
 // }
-});
+
