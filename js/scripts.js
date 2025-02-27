2d8119b0831ecd8533b2e49e0119ef01e0362f97
LindsellR <lindsellrichard@gmail.com>
2025-02-27 17:13:29 +1100

    index.html, scripts.js
    index.html
    added polyfills for promises and fetch.
    
    scripts.js
    removed old pokemonList array and added pokemon apiUrl.
    amended add function conditional to include detailsUrl from pokemon API.
    Added loadList function to fetch list of pokemon.
    Added loadDetails function to get pokemon details.
    Assigned name, detailsUrl, Height and type details.
    Assigned loadList and LoadDetails to same name keywords to access pokemon repository

--- js/scripts.js
+++ js/scripts.js
@@ -1,64 +1,132 @@
 //Pokemon Array wrapped in IIFE
 
 let pokemonRepository = (function () {
-  let pokemonList = [
-    { name: 'Bulbasaur', height: 7, type: ['grass, poison'] },
-    { name: 'Butterfree', height: 3, type: ['bug', 'flying'] },
-    { name: 'Charizard', height: 5, type: ['fire', 'flying'] },
-  ];
+  let pokemonList = [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
+// Function to add pokemon items if consitions are met!! 
+function add(pokemon) {
+  if (pokemon && typeof pokemon === 'object' && 
+    'name' in pokemon  &&
+    'detailsUrl' in pokemon){
+    pokemonList.push(pokemon);
+  } else {
+    console.log('Invalid input: $Json.stringify(pokemon) is not a valid pokemon');
+  }
+}
+
+function getAll() {
+  return pokemonList;
+}
   //Function to add list item button
-  function addListItem(pokemon){
+function addListItem(pokemon){
     let listContainer = document.querySelector('.pokemon-list');
     let listItem = document.createElement('li');
     let button = document.createElement('button');
     button.innerText = pokemon.name;
     button.classList.add('pokeButton');
     listItem.appendChild(button);
-    listContainer.appendChild(listItem)
-
+    listContainer.appendChild(listItem);
+    //To remove bullet points from list
+    listContainer.style.listStyleType = ('none')
     button.addEventListener('click', function(){
-      showDetails(pokemon);
-  });
+    showDetails(pokemon);
+});
+}
 
-  //To remove bullet points from list
-  listContainer.style.listStyleType = 'none';
+  //To access pokemon from API
+  function loadList() {
+    return fetch(apiUrl).then(function (response) {
+      return response.json();
+})
+.then(function (json) {
+    json.results.forEach(function (item) {
+      let pokemon = {
+        name: item.name,
+        detailsUrl: item.url
+};
+    add(pokemon);
+});
+})  
+.catch(function (e) {
+  console.error(e);
+})
+}
 
+// Function to load pokemon details
+function loadDetails(item) {
+  let url = item.detailsUrl;
+  return fetch(url).then(function (response) {
+  return response.json();
+  })
+  .then(function (details) {
+    item.imageUrl = details.sprites.front_default;
+    item.height = details.height;
+    item.types = details.types;
+})  
+.catch(function (e) {
+  console.error(e);
+});
+}
     //function to show pokemon details
-   function showDetails(pokemon){
-     console.log(pokemon);
-  }
-  }
-  // Function to add pokemon items if consitions are met.
-    function add(pokemon) {
-     if (pokemon && typeof pokemon === 'object' && 'name' in pokemon && 'height' in pokemon && 'type' in pokemon)
-    {
-      pokemonList.push(pokemon);
-     } else {
-       console.log('Invalid input: $Json.stringify(pokemon) is not a valid pokemon');
-     }
-   }
-  
-  function getAll() {
-    return pokemonList;
-  }
-
-  // to make pokemon list accessible outside of function
+function showDetails(pokemon) {
+  loadDetails(pokemon).then(function () {
+    console.log(pokemon);
+  });
+}
+      
+   // to make pokemon list accessible outside of function
   return {
     add: add,
     getAll: getAll,
     addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails,
+    showDetails: showDetails
   };
+
 })();
-//adding pokemon to list if conditions are met
-pokemonRepository.add({ name: "Pikachu", height: 0.3, type: ["electric"] });
 
-//forEach loop over pokemon array
-pokemonRepository.getAll().forEach(function (pokemon) {
-  pokemonRepository.addListItem(pokemon)
-});
-
+// Load data - adListItem loaded last.
+pokemonRepository.loadList().then(function(){
+  pokemonRepository.getAll().forEach(function(pokemon){
+    pokemonRepository.addListItem(pokemon);
+  });
+})
+  
 // *SUPRECEDED CODE!
+//   // Function to add pokemon items if consitions are met!!
+//     function add(pokemon) {
+//      if (pokemon && typeof pokemon === 'object' && 'name' in pokemon && 'height' in pokemon && 'type' in pokemon)
+//     {
+//       pokemonList.push(pokemon);
+//      } else {
+//        console.log('Invalid input: $Json.stringify(pokemon) is not a valid pokemon');
+//      }
+//    }
+  
+//   function getAll() {
+//     return pokemonList;
+//   }
+
+//   // to make pokemon list accessible outside of function!!
+//   return {
+//     add: add,
+//     getAll: getAll,
+//     addListItem: addListItem,
+//   };
+
+  
+// })();
+// //adding pokemon to list if conditions are met!!
+// pokemonRepository.add({ name: "Pikachu", height: 0.3, type: ["electric"] });
+
+// //forEach loop over pokemon array!!
+// pokemonRepository.getAll().forEach(function (pokemon) {
+//   pokemonRepository.addListItem(pokemon)
+// });
+
+
 // //loops through pokemonList variable
 // for (let i = 0; i < pokemonList.length; i++) {
 //   //extracts certain data and prints to DOM. html tags added for styling
